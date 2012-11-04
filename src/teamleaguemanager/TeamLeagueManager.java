6be985b395a2e4ac157cb9b7db4ed3a2b38508e9
fazevedodev <fazevedo.dev@gmail.com>
2012-11-03 23:05:42 -0200

    Projeto BW Toolbox

---
+++ src/teamleaguemanager/TeamLeagueManager.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teamleaguemanager;
+
+import teamleaguemanager.gui.MainView;
+
+/**
+ *
+ * @author Fabiano
+ */
+public class TeamLeagueManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MainView().setVisible(true);
+    }
+}
